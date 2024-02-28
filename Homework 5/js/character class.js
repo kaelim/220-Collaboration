8576ef8a49ed710194ca5097f37909706c604610
Mhanon-Sullivan <ms188484@umconnect.umt.edu>
2024-02-28 11:14:34 -0700

    Homework 7 Update

--- Homework 5/js/character class.js
+++ Homework 5/js/character class.js
@@ -1,101 +1,85 @@
 
 class Character {
-  constructor(image, x, y) {
-    this.image = image;
+  constructor(runStrings, idleStrings, x, y, w, h) {
     this.x = x;
     this.y = y;
+    this.w = w;
+    this.h = h;
+    this.runningCycle = [];
+    this.idleCycle = [];
+    // load all the images here
+    for (var k = 0; k < runStrings.length; k++) {
+      this.runningCycle[k] = loadImage(runStrings[k]);
+    }
+    for (var k = 0; k < idleStrings.length; k++) {
+      this.idleCycle[k] = loadImage(idleStrings[k]);
+    }
+    //this.idleArray();
+    //this.resizeCharacter();
 
   }
 
-  resizeCharacter () {
+  // use the w and h sent in
+  resizeCharacter() {
 
-    for(var i = 0; i < runningCycle.length; i++)
-  {
-      runningCycle[i].resize(runningCycle[i].width / 5, 0);
+    for (var j = 0; j < idleCycle.length; j++) {
+
+      // idleCycle[j].resize(idleCycle[j].width / 5, 0);
+      idleCycle[j].resize(this.w / 2, this.h / 2);
+    }
+
   }
-  
-  for(var j = 0; j < idleCycle.length; j++)
-  {
-      idleCycle[j].resize(idleCycle[j].width / 5, 0);
-  }
-  
-  }
+
+  /*resizeRunning() {
+   
+    for (var ii = 0; ii < this.runningCycle.length; ii++) {
+     
+    this.runningCycle[ii].resize(136,0);
+    }
+  }*/
 
   drawCharacter() {
 
-   
-    
-   if(keyIsPressed)
-   {
-     if(key == "w")
-     {
+    // resize using the w and h from the constructor
+    for (var k = 0; k < this.runningCycle.length; k++) {
+      this.runningCycle[k].resize(this.w / 2, this.h / 2);
+    }
+    for (var k = 0; k < this.idleCycle.length; k++) {
+      this.idleCycle[k].resize(this.w / 2, this.h / 2);
+    }
+
+    if (keyIsPressed) {
+
+      if (key == "w") {
         yImage -= 1;
-     }
-     if(key == "s")
-     {
+      }
+      if (key == "s") {
         yImage += 1;
-     }
-     if(key == "a")
-     {
+      }
+      if (key == "a") {
         xImage -= 1;
         flipX = true;
-     }
-     if(key == "d")
-     {
+      }
+      if (key == "d") {
         xImage += 1;
         flipX = false;
-     }
+      }
 
-     if(flipX)
-     {
-         push();
-         
-         scale(-1, 1);
- 
-         image(runningCycle[i], -xImage-runningCycle[i].width/2, yImage);
- 
-         pop();
-     }
-     else
-     {
-         image(runningCycle[i], xImage, yImage);
-     }
+      if (flipX) {
+        push();
 
-  } else{
+        scale(-1, 1);
+        image(this.runningCycle[i], -xImage - this.w / 2, yImage);
 
-    image(idleCycle[i], xImage, yImage);
+        pop();
+      } else {
 
+        image(this.runningCycle[i], xImage, yImage);
+      }
+
+    } else {
+      image(this.idleCycle[i], xImage, yImage);
+
+    }
   }
-
-  }
-
-  runningArray () {
-
-runningCycle[0] = run1;
-runningCycle[1] = run2;
-runningCycle[2] = run3;
-runningCycle[3] = run4;
-runningCycle[4] = run5;
-runningCycle[5] = run6;
-runningCycle[6] = run7;
-runningCycle[7] = run8;
-
-  }
-
-  idleArray () {
-
-idleCycle[0] = idle1;
-idleCycle[1] = idle2;
-idleCycle[2] = idle3;
-idleCycle[3] = idle4;
-idleCycle[4] = idle5;
-idleCycle[5] = idle6;
-idleCycle[6] = idle7;
-idleCycle[7] = idle8;
-idleCycle[8] = idle9;
-idleCycle[9] = idle10;
-console.log(idleCycle.length)
-  }
-
-
 }
--- Homework 5/js/sketch2.js
+++ Homework 5/js/sketch2.js
@@ -1,135 +1,87 @@
-var run1
-var run2;
-var run3;
-var run4;
-var run5;
-var run6;
-var run7;
-var run8;
+var myFood;
+var myCharacter;
 
-var idle1;
-var idle2;
-var idle3;
-var idle4;
-var idle5;
-var idle6;
-var idle7;
-var idle8;
-var idle9;
-var idle10;
-
-var myFood
-var myCharacter
-
-var xImage = 100, yImage = 25;
+var xImage = 100,
+  yImage = 25;
 var speedX, speedY;
 
 var i = 0;
 var j = 0;
+var f = 1;
 
-var runningCycle = [];
-var idleCycle = [];
+var runningCycle1 = [];
+var idleCycle1 = [];
 
 var flipX = false;
 
+var score = 0
 var time = 100;
 var textX = 30;
 var textY = 40;
-  setInterval(timeIt, 1000);
 
-
-var runStrings
+let runStrings;
+let idleStrings;
 
 function preload() {
-
-  
-  runStrings = loadStrings('run.txt');
-  console.log("hi" + runStrings.length)
-/*
-  idle1 = loadImage("./images/Idle (1).png");
-  idle2 = loadImage("./images/Idle (2).png");
-  idle3 = loadImage("./images/Idle (3).png");
-  idle4 = loadImage("./images/Idle (4).png");
-  idle5 = loadImage("./images/Idle (5).png");
-  idle6 = loadImage("./images/Idle (6).png");
-  idle7 = loadImage("./images/Idle (7).png");
-  idle8 = loadImage("./images/Idle (8).png");
-  idle9 = loadImage("./images/Idle (9).png");
-  idle10 = loadImage("./images/Idle (10).png");
-*/
+  runStrings = loadStrings('images/run.txt');
+  idleStrings = loadStrings('images/idle.txt');
 }
 
 function setup() {
-  createCanvas(500, 500);
 
+  createCanvas(800, 600);
 
+  myFood = new Food(random(10, width), random(20, height), random(20, 80));
+  
+  myCharacter = new Character(runStrings, idleStrings, 100, 100, 680, 472);
 
-  for(var k = 0; k < runStrings.length; k++){
-    console.log("hi")
-    runningCycle.push(loadImage(runStrings[k]));
-    
-      } 
+  setInterval(changeTime, 100);
 
-myFood = new Food(random(10, width), random(20, height), random(20, 80));
-myCharacter = new Character(idleCycle = [j], 100, 100);
-
-myCharacter.idleArray();
-//myCharacter.runningArray();
-
-myCharacter.resizeCharacter();
-
-setInterval(changeTime, 10)
-
-
+  setInterval(timeIt, 1000);
 
 }
 
 function draw() {
-  background(220);
-
+  background(200);
   myFood.drawFood();
 
-    fill(25,0,170);
-    textSize(24);
-    text("Time Left: " + time, textX, textY);
+  fill(25, 0, 170);
+  textSize(24);
+  text("Time Left: " + time, textX, textY);
+  text("Score: " + score, textX + 200, textY);
 
   myCharacter.drawCharacter();
 
-    //collision
-    let d = dist(xImage, yImage, myFood.x, myFood.y);
+  //collision
+  let d = dist(xImage + 100, yImage + 100, myFood.x, myFood.y);
 
-    if (d < 120) {
-  
-      myFood.x = 1000
-      myFood.y = 1000
-  
-    }
+  if (d < 150) {
+
+    myFood.x = random(width - 90);
+    myFood.y = random(height - 90);
+    score = ++f;
+  }
 
 }
 
-function changeTime()
+function changeTime() {
 
-{
+  i++;
+  if (i > runStrings.length - 1) {
+    i = 0;
+  }
 
-    i++;
-    if(i > runningCycle.length-1)
-    {
-        i = 0;
-    }
-
-    j++;
-    if(j > idleCycle.length-1)
-    {
-        j = 0;
-    }
+  j++;
+  if (j > idleStrings.length - 1) {
+    j = 0;
+  }
 }
 
 function timeIt() {
   if (time > 0) {
     time--;
+  } else {
+    time = 100;
   }
-  else
-  {
-      time = 100;
-  }
+ 
 }
